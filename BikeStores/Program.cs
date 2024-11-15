b394973b54fa373678662e0a6b703a3fcbdc7537
Ahmed Naeim <eng.a.naeim@gmail.com>
2024-11-15 22:22:29 +0200

    Add project files.

---
+++ BikeStores/Program.cs
@@ -0,0 +1,20 @@
+﻿using BikeStores.Data;
+using BikeStores.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeStores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+
+            var orders = dbContext.Orders.Include(e=> e.Staff).ThenInclude(e=>e.Store).ToList();
+
+            foreach (var item in orders) {
+                
+            }
+        }
+    }
+}
